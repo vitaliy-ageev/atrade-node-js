c1dae412ffb486a2e378652163612653ea69292d
Агеев Виталий <ageev.vitaly@rktv.ru>
2022-02-18 18:16:12 +0500

    Описали модель, настроили обработчик веб запросов

--- server/index.js
+++ server/index.js
@@ -4,10 +4,18 @@ require('dotenv').config();
 const express = require('express');
 // импортируем объект подключения к БД
 const sequelize = require('./db');
+// импортируем модель
+const models = require('./models/models');
+// Импортируем Корс для веб запросов
+const cors = require('cors');
 // импортируем порт из настроек .env
 const PORT = process.env.PORT || 5000;
 // объявляем переменную приложения
 const app = express();
+// Настраиваем обработку веб запросов с браузера
+app.use(cors());
+// Разрешаем парсить json формат
+app.use(express.json());
 
 const start = async () => {
     try {
--- server/models/models.js
+++ server/models/models.js
@@ -20,10 +20,88 @@ const BasketDevice = sequelize.define('basket_device', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
 });
 
+// Модель девайсов
 const Device = sequelize.define('device', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    name: {type: DataTypes.STRING, unique: true, allowNull: false},
+    price: {type: DataTypes.INTEGER, allowNull: false},
+    rating: {type: DataTypes.INTEGER, defaultValue: 0},
+    img: {type: DataTypes.STRING, allowNull: false},
+});
+
+// Модель типов
+const Type = sequelize.define('type', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+});
+
+// Модель брендов
+const Brand = sequelize.define('brand', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     name: {type: DataTypes.STRING, unique: true, allowNull: false},
 });
 
+// Модель рейтингов
+const Rating = sequelize.define('rating', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    rate: {type: DataTypes.INTEGER, allowNull: false},
+});
+
+// Модель информации о девайсах
+const DeviceInfo = sequelize.define('device_info', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    title: {type: DataTypes.STRING, allowNull: false},
+    description: {type: DataTypes.STRING, allowNull: false},
+});
+
+const TypeBrand = sequelize.define('type_brand', {
+   id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+});
+
+ //-------- Связываем модели друг с другом
+// Девайсы содержат много записей с характиристиками
+Device.hasMany(DeviceInfo);
+// Информация о девайсах принадлежит Девайсам
+DeviceInfo.belongsTo(Device);
+
+// Пользователь один к одному (одна корзина для каждого пользователя)
+User.hasOne(Basket);
+Basket.belongsTo(User);
+
+User.hasMany(Rating);
+Rating.belongsTo(User);
+
+Basket.hasMany(BasketDevice);
+BasketDevice.belongsTo(Basket);
+
+Type.hasMany(Device);
+Device.belongsTo(Type);
+
+Brand.hasMany(Device);
+Device.belongsTo(Brand);
+
+Device.hasMany(Rating);
+Rating.belongsTo(Device);
+
+Device.hasMany(BasketDevice);
+BasketDevice.belongsTo(Device);
+
+Device.hasMany(DeviceInfo);
+DeviceInfo.belongsTo(Device);
+
+// Опишем тип связи между типом и брендом (многим ко многим)
+Type.belongsToMany(Brand, {through: TypeBrand});
+Brand.belongsToMany(Type, {through: TypeBrand});
+
+module.exports = {
+    User,
+    Basket,
+    BasketDevice,
+    Device,
+    Type,
+    Brand,
+    Rating,
+    TypeBrand,
+    DeviceInfo
+};
 
 
